297bd861d073482062ed0607e6926c432c1bafb6
GaboJHSP <104600151+GaboJHSP@users.noreply.github.com>
2022-11-07 19:12:27 -0400

    Version 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavmenuComponent,
+    IndexComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
