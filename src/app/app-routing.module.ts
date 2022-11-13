6af7093a8153c838dedb646e9b96a55f4dcdcc1f
GaboJHSP <104600151+GaboJHSP@users.noreply.github.com>
2022-11-12 21:25:49 -0400

    Version 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { InfantilComponent } from './infantil/infantil.component';
+import { InscripcionComponent } from './inscripcion/inscripcion.component';
 import { IntensivoComponent } from './intensivo/intensivo.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 import { RegularComponent } from './regular/regular.component';
 
 const routes: Routes = [
@@ -10,7 +12,9 @@ const routes: Routes = [
   { path: 'index', component: IndexComponent },
   { path: 'regular', component: RegularComponent },
   { path: 'intensivo', component: IntensivoComponent },
-  { path: 'infantil', component: InfantilComponent }
+  { path: 'infantil', component: InfantilComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'inscripcion', component: InscripcionComponent }
 ];
 
 @NgModule({
